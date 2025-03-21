cf5434386d5e086664515da6abc71075502bc9d6
KamatchiKarthi <karthivdj@gmail.com>
2025-03-21 16:21:16 +0530

    landing page -2

--- js/script.js
+++ js/script.js
@@ -77,11 +77,11 @@
 // Simulating rolling a dice multiple times
 // Creating a simple pattern of stars
 
-const a = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    resolve([1, 2, 3, 3, 4]);
-  }, 5000);
-});
+// const a = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve([1, 2, 3, 3, 4]);
+//   }, 5000);
+// });
 
 // a.then(result => {
 //   console.log('result is', result);
@@ -93,10 +93,186 @@ const a = new Promise(function (resolve, reject) {
 //     console.log('finally');
 //   });
 
+// async function finalcall() {
+//   let last = await a;
+//   console.log(last);
+// }
+// finalcall();
 
+// try {
+//   console.log('succes');
+// } catch {
+//   console.log(error);
+// }
+// console.log('final');
 
-async function finalcall() {
-  let last = await a;
-  console.log(last);
-}
-finalcall();
+// let urr = 'https://jsonplaceholder.typicode.com/todos/';
+
+// fetch(urr)
+//   .then(response => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// async function getdata() {
+//   let last = await fetch(urr);
+//   let result = await last.json();
+//   console.log(result);
+// }
+
+// async function getdata() {
+//   try {
+//     let last = await fetch(urr);
+//     let result = await last.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log('error');
+//   }
+// }
+// getdata();
+// console.log('completed');
+
+// let urii = 'https://tasks-fs.onrender.com/todos';
+// let createuii = "https://tasks-fs.onrender.com/todos/create"
+
+// fetch(createuii, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body:JSON.stringify({
+//     id :3,
+//     title: "fix headers"
+//   })
+// })
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// let book = {
+//   title: 'vijay',
+//   authro: 'thalapathy',
+//   info: function () {
+//     console.log(`${this.title} by ${this.authro}`);
+//   },
+// };
+
+// book.info();
+document.addEventListener('DOMContentLoaded', () => {
+  const gameBoard = document.querySelector('.game-board');
+  const restartButton = document.getElementById('restartButton');
+  let cards = [];
+  let flippedCards = [];
+  let gameLocked = false;
+
+  // Array of card values (pairs)
+  const cardValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  let totalPairs = cardValues.length;
+  let pairsFound = 0;
+
+  function createCards() {
+      // Double the card values to create pairs
+      const duplicatedValues = [...cardValues, ...cardValues];
+      // Shuffle the array
+      shuffleArray(duplicatedValues);
+
+      cards = duplicatedValues.map(value => {
+          const card = document.createElement('div');
+          card.classList.add('card');
+          card.dataset.value = value;
+
+          const cardInner = document.createElement('div');
+          cardInner.classList.add('card-inner');
+
+          const cardFront = document.createElement('div');
+          cardFront.classList.add('card-front');
+          cardFront.textContent = 'X';
+
+          const cardBack = document.createElement('div');
+          cardBack.classList.add('card-back');
+          cardBack.textContent = value;
+
+          cardInner.appendChild(cardFront);
+          cardInner.appendChild(cardBack);
+          card.appendChild(cardInner);
+          card.addEventListener('click', flipCard);
+          return card;
+      });
+  }
+
+  function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  }
+
+  function renderBoard() {
+      gameBoard.innerHTML = '';
+      cards.forEach(card => {
+          gameBoard.appendChild(card);
+      });
+  }
+
+  function flipCard() {
+      if (gameLocked || this === flippedCards[0] || this.classList.contains('flipped')) {
+          return;
+      }
+
+      this.classList.add('flipped');
+      flippedCards.push(this);
+
+      if (flippedCards.length === 2) {
+          gameLocked = true;
+          checkForMatch();
+      }
+  }
+
+  function checkForMatch() {
+      const card1 = flippedCards[0];
+      const card2 = flippedCards[1];
+
+      if (card1.dataset.value === card2.dataset.value) {
+          // Match found
+          card1.removeEventListener('click', flipCard);
+          card2.removeEventListener('click', flipCard);
+          flippedCards = [];
+          gameLocked = false;
+          pairsFound++;
+          if (pairsFound === totalPairs) {
+              alert('Congratulations! You found all the pairs!');
+          }
+      } else {
+          // No match
+          setTimeout(() => {
+              card1.classList.remove('flipped');
+              card2.classList.remove('flipped');
+              flippedCards = [];
+              gameLocked = false;
+          }, 1000); // Flip back after 1 second
+      }
+  }
+
+  function restartGame() {
+      flippedCards = [];
+      gameLocked = false;
+      pairsFound = 0;
+      createCards();
+      renderBoard();
+  }
+
+  restartButton.addEventListener('click', restartGame);
+
+  // Initial game setup
+  createCards();
+  renderBoard();
+});
---
+++ memory game/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const memoryboard = document.querySelector('.memory-board');
+  const restartbtn = document.getElementById('restartbtn');
+  let cards = [];
+  let flippedcards = [];
+  let cardpause = false;
+  
+});
